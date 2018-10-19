5dfa0c8560c0f911928c6795881494b274db98d9
Jake Plahn <jake@jakeplahn.com>
2018-10-19 16:38:26 -0700

    Update setup_minikube_from_host.sh

--- setup_minikube_from_host.sh
+++ setup_minikube_from_host.sh
@@ -1,3 +1,4 @@
 sed -i '' 's/vb.memory = "1024"/vb.memory = "6144"/' Vagrantfile
 vagrant reload
+vagrant ssh -c "chmod +x ~/repos/vms/setup_minikube.sh"
 vagrant ssh -c "~/repos/vms/setup_minikube.sh"
