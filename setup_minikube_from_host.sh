bfad9ac933ac433477f3e7a1197a2d9d4c76e688
Jake Plahn <jake@jakeplahn.com>
2018-10-19 16:36:58 -0700

    Update setup_minikube_from_host.sh

--- setup_minikube_from_host.sh
+++ setup_minikube_from_host.sh
@@ -1,7 +1,3 @@
-sed -i -e '/Vagrant.configure("2") do |config|/a\'$'\n''\ \ config.vm.provider "vmware_fusion" do |vb|\
-\ \ \ \ vb.memory = "6144"\
-\ \ \ \ vb.clone_directory = "."\
-\ \ end\
-' Vagrantfile
+sed -i '' 's/vb.memory = "1024"/vb.memory = "6144"/' Vagrantfile
 vagrant reload
 vagrant ssh -c "~/repos/vms/setup_minikube.sh"
