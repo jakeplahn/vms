8dfca1eee725c70f7fec3a9561dc59a251df3a91
Jake Plahn <jake@jakeplahn.com>
2018-10-30 04:44:00 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sed -i -e '/Vagrant.configure("2") do |config|/a\'$'\n''\ \ config.vm.provider "
 \ \ end\
 ' Vagrantfile
 vagrant up --provider vmware_fusion
-vagrant ssh -c "sudo apt-get update && sudo apt-get upgrade"
+vagrant ssh -c "sudo apt-get update && sudo apt-get -y upgrade"
 vagrant ssh -c "mkdir ~/repos"
 vagrant ssh -c "git clone https://github.com/jakeplahn/vms.git ~/repos/vms"
 vagrant ssh -c "chmod +x ~/repos/vms/setupvm.sh"
