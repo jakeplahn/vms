bfe4e8caece87454dd27bb59f20ccec51fe1b423
Jake Plahn <jake@jakeplahn.com>
2018-12-11 04:29:02 -0800

    Update setup_minikube.sh

--- setup_minikube.sh
+++ setup_minikube.sh
@@ -10,5 +10,6 @@ rm minikube
 sudo apt-get -y install gcc make linux-headers-$(uname -r) dkms
 sudo apt-get -y install virtualbox
 sudo apt-get install bash-completion
+echo "source <(kubectl completion bash)" >> ~/repos/vms/.bash_customizations
 minikube start --memory 4096
 kubectl get services
