7def473c9292321e5a7a8d90c680db301b6edba4
Jake Plahn <jake@jakeplahn.com>
2019-06-04 12:07:30 -0700

    Update setup_nginx.sh

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,4 +1,4 @@
-sudo sh -c "echo 'deb http://nginx.org/packages/ubuntu/ cosmic nginx '$(lsb_release -cs)' nginx' > /etc/apt/sources.list.d/nginx.list"
+sudo sh -c "echo 'deb http://nginx.org/packages/ubuntu/ '$(lsb_release -cs)' nginx' > /etc/apt/sources.list.d/nginx.list"
 sudo sh -c "echo 'deb-src http://nginx.org/packages/ubuntu/ '$(lsb_release -cs)' nginx' >> /etc/apt/sources.list.d/nginx.list"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ABF5BD827BD9BF62
 sudo apt-get update
