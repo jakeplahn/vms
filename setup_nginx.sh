c2f08fa0ffb19ff49c1d2e0756f333994ef22a7f
Jake Plahn <jake@jakeplahn.com>
2019-06-04 11:39:28 -0700

    Update setup_nginx.sh

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,7 +1,6 @@
-cd /tmp/ && wget http://nginx.org/keys/nginx_signing.key
-sudo apt-key add nginx_signing.key
 sudo sh -c "echo 'deb http://nginx.org/packages/stable/ubuntu/ '$(lsb_release -cs)' nginx' > /etc/apt/sources.list.d/nginx.list"
 sudo sh -c "echo 'deb-src http://nginx.org/packages/ubuntu/ '$(lsb_release -cs)' nginx' >> /etc/apt/sources.list.d/nginx.list"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ABF5BD827BD9BF62
 sudo apt-get update
 sudo apt-get install nginx
 nginx -v
