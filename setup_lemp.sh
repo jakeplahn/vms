6e8d5faf5d3b3187354187da23ec5c6cc7e4bcbb
Jake Plahn <jake@jakeplahn.com>
2019-06-04 12:30:47 -0700

    Update setup_lemp.sh

--- setup_lemp.sh
+++ setup_lemp.sh
@@ -3,6 +3,13 @@ sudo sh -c "echo 'deb-src http://nginx.org/packages/ubuntu/ '$(lsb_release -cs)'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ABF5BD827BD9BF62
 sudo apt-get update
 sudo apt-get install nginx
+#
+sudo apt-get install software-properties-common
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.3/ubuntu cosmic main'
+sudo apt update
+sudo apt install mariadb-server
+#
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt install php7.3 php7.3-common php7.3-cli
