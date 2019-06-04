e146ffe35f085b74f2d72075c49978c388185426
Jake Plahn <jake@jakeplahn.com>
2019-06-04 12:29:57 -0700

    Update setup_lemp.sh

--- setup_lemp.sh
+++ setup_lemp.sh
@@ -1,3 +1,8 @@
+sudo sh -c "echo 'deb http://nginx.org/packages/ubuntu/ '$(lsb_release -cs)' nginx' > /etc/apt/sources.list.d/nginx.list"
+sudo sh -c "echo 'deb-src http://nginx.org/packages/ubuntu/ '$(lsb_release -cs)' nginx' >> /etc/apt/sources.list.d/nginx.list"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ABF5BD827BD9BF62
+sudo apt-get update
+sudo apt-get install nginx
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt install php7.3 php7.3-common php7.3-cli
