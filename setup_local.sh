688d9858b1a3690f76d9674fbd52299f3e10bff4
Jake Plahn <jake@jakeplahn.com>
2019-06-04 10:08:06 -0700

    Update setup_local.sh

--- setup_local.sh
+++ setup_local.sh
@@ -1,1 +1,9 @@
-#
+vagrant init ubuntu-18-1-0-local
+sed -i -e '/Vagrant.configure("2") do |config|/a\'$'\n''\ \ config.vm.provider "vmware_fusion" do |vb|\
+\ \ \ \ vb.memory = "1024"\
+\ \ \ \ vb.clone_directory = "."\
+\ \ end\
+' Vagrantfile
+vagrant up --provider vmware_fusion
+vagrant ssh -c "sudo apt-get update && sudo apt-get -y upgrade"
+vagrant ssh -c "mkdir ~/repos"
