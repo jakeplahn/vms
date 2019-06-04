fa1531d4e4607392c47bed6719ba5c2b6f21215d
Jake Plahn <jake@jakeplahn.com>
2019-06-04 12:11:26 -0700

    Update setup_mariadb.sh

--- setup_mariadb.sh
+++ setup_mariadb.sh
@@ -1,1 +1,5 @@
-
+sudo apt-get install software-properties-common
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.3/ubuntu cosmic main'
+sudo apt update
+sudo apt install mariadb-server
