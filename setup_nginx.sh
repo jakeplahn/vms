38c70cb91f7972603c478e2929ded0e71428169b
Jake Plahn <jake@jakeplahn.com>
2019-06-05 09:00:20 -0700

    Delete setup_nginx.sh

--- setup_nginx.sh
+++
@@ -1,6 +0,0 @@
-sudo sh -c "echo 'deb http://nginx.org/packages/ubuntu/ '$(lsb_release -cs)' nginx' > /etc/apt/sources.list.d/nginx.list"
-sudo sh -c "echo 'deb-src http://nginx.org/packages/ubuntu/ '$(lsb_release -cs)' nginx' >> /etc/apt/sources.list.d/nginx.list"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ABF5BD827BD9BF62
-sudo apt-get update
-sudo apt-get install nginx
-nginx -v
