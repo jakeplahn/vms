605d85ee3719fce58aa5db6529cf51ea58ffc4d8
Jake Plahn <jake@jakeplahn.com>
2019-07-10 07:10:05 -0700

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -12,6 +12,6 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 sudo usermod -aG docker vagrant
 docker run hello-world
